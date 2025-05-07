745331715363fda434eb584828b77ebad382847c
BalusuDivya <168392369+BalusuDivya@users.noreply.github.com>
2025-05-07 16:08:03 +0530

    Add files via upload

---
+++ Random Background Color Changer/index.js
@@ -0,0 +1,6 @@
+function myfunction() {
+    var red = Math.random() * 255;
+    var green = Math.random() * 255;
+    var blue = Math.random() * 255;
+    document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
